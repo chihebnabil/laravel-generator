29e9c4a3df4ec73c54b9accbf03ef2b0771d889e
Chiheb Nabil <chiheb.design@gmail.com>
2019-12-04 21:46:00 +0100

    update composer.json

--- src/Commands/BuildCommand.php
+++ src/Commands/BuildCommand.php
@@ -16,6 +16,15 @@ class BuildCommand extends Command
 
     protected $description = 'Generate Laravel Module from a YAML file';
 
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
     public function handle()
     {
